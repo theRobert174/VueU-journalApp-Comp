9c583ef4ea7264585ec6584e382abc08e1268924
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-04-30 03:44:15 -0600

    interceptors in axios requests

--- src/api/journalApi.js
+++ src/api/journalApi.js
@@ -4,4 +4,11 @@ const journalApi = axios.create({
     baseURL: 'https://vue-journal-69cbb-default-rtdb.firebaseio.com'
 })
 
+journalApi.interceptors.request.use((config) => {
+    config.params = {
+        auth: localStorage.getItem('idToken')
+    }
+    return config
+})
+
 export default journalApi
