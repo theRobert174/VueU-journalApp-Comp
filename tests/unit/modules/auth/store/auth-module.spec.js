a88dcf6de18ac17cee570152a3b68dca78306bb9
theRobert174 <rodrigomtzmacias@hotmail.com>
2023-05-28 12:11:47 -0600

    test Mutations

--- tests/unit/modules/auth/store/auth-module.spec.js
+++ tests/unit/modules/auth/store/auth-module.spec.js
@@ -18,4 +18,53 @@ describe('Vuex: Pruebas en el auth-modules', () => {
         expect(idToken).toBe(null)
         expect(refreshToken).toBe(null)
     })
+
+    // Mutations
+    test('Mutation: loginUser', () => {
+        const store = createVuexStore({
+            status: 'authenticating', //'authenticated','not-authenticated','authenticating'
+            user: null,
+            idToken: null,
+            refreshToken: null
+        })
+
+        const payload = {
+            user: { name: 'Rodrigo', email: 'rodrigo@renderToString.com'},
+            idToken: '123-ABC',
+            refreshToken: '456-ABC'
+        }
+
+        store.commit('auth/loginUser', payload )
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+
+        expect(status).toBe('authenticated')
+        expect(user).toEqual({ name: 'Rodrigo', email: 'rodrigo@renderToString.com'})
+        expect(idToken).toBe('123-ABC')
+        expect(refreshToken).toBe('456-ABC')
+    })
+
+    test('Mutation: logout', () => {
+        const store = createVuexStore({
+            status: 'authenticated', //'authenticated','not-authenticated','authenticating'
+            user: 'test',
+            idToken: '123456789',
+            refreshToken: '987654321'
+        })
+
+        localStorage.setItem('idToken','123456789')
+        localStorage.setItem('refreshToken','987654321')
+
+        store.commit('auth/logout')
+
+        const { status, user, idToken, refreshToken } = store.state.auth
+
+        expect(status).toBe('not-authenticated')
+        expect(user).toBeFalsy()
+        expect(idToken).toBeFalsy()
+        expect(refreshToken).toBeFalsy()
+
+        expect(localStorage.getItem('idToken')).toBeFalsy()
+        expect(localStorage.getItem('refreshToken')).toBeFalsy()
+    })
 })
